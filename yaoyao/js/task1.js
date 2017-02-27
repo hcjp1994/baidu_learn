173fdf47f5988b2f7c05033c6b0e0793adce83b7
hcjp1994 <1315922245@qq.com>
2017-02-27 16:37:19 +0800

    task

---
+++ yaoyao/js/task1.js
@@ -0,0 +1,25 @@
+window.onload=function () {
+  var name=document.getElementById("name");
+  var password=document.getElementById("password");
+  var div=document.getElementsByTagName("div");
+  var span=document.getElementsByTagName("span");
+  var btn=document.getElementsByTagName("button");
+  //验证姓名不能为空
+  btn[0].onclick=function() {
+    if(name.value==""||name.value==null){
+      span[0].innerHTML="姓名不能为空！";
+    }
+    else {
+      span[0].innerHTML="";
+    }
+  }
+  btn[1].onclick=function () {
+    var reg=/\.{4,16}/
+    if(!reg.test(password.value)){
+      span[1].innerHTML="必填，长度为4-16位任意字符";
+    }
+    else {
+      span[1].innerHTML="";
+    }
+  }
+}
