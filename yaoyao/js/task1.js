dd5e84a1cf194acc3653f331412e79f51414396e
hcjp1994 <1315922245@qq.com>
2017-02-28 13:40:59 +0800

    task

--- yaoyao/js/task1.js
+++ yaoyao/js/task1.js
@@ -15,7 +15,7 @@ window.onload=function () {
   }
   // 验证输入长度为4-16位之间
   btn[1].onclick=function () {
-    var reg=/\.{4,16}/
+    var reg=/\.{4,16}/;
     if(!reg.test(password.value)){
       span[1].innerHTML="必填，长度为4-16位任意字符";
     }
